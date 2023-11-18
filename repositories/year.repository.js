072037641a1a8e83ae76ec811cbda0d698508311
villani <felipevillani@gmail.com>
2023-11-17 22:27:09 -0300

    add order year

--- repositories/year.repository.js
+++ repositories/year.repository.js
@@ -3,7 +3,7 @@ import { connect } from "./db.js";
 async function getYearRepository() {
     const conn = await connect();
     try {
-        const res = await conn.query("SELECT year FROM city group by year");
+        const res = await conn.query("SELECT year FROM city group by year order by year desc");
 
         return res.rows;
     } catch (err) {
