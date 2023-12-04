3aa2b5a962f4418e467c6979c7827356affee647
villani <felipevillani@gmail.com>
2023-12-03 23:03:15 -0300

    adjust

--- controllers/type.controllers.js
+++ controllers/type.controllers.js
@@ -2,7 +2,6 @@ import getTypeService from "../services/type.service.js";
 
 async function getTypeController(req, res, next) {
     try {
-        console.log("req.body", req.body);
         res.send(await getTypeService(req.body.paramTheme, req.body.paramType));
     } catch (err) {
         next(err);
--- repositories/type.repository.js
+++ repositories/type.repository.js
@@ -3,8 +3,6 @@ import { connect } from "./db.js";
 async function getTypeRepository(paramTheme, paramType) {
     const conn = await connect();
     try {
-        console.log("paramTheme", paramTheme);
-        console.log("paramType", paramType);
         const res = await conn.query("SELECT * FROM city where theme = $1 and type = $2", [paramTheme, paramType]);
 
         return res.rows;
