6f78b6af4b4323228e9381bfbcbaa79301e28b91
villani <felipevillani@gmail.com>
2023-12-03 22:39:45 -0300

    update

--- routes/type.router.js
+++ routes/type.router.js
@@ -3,6 +3,6 @@ import getTypeController from "../controllers/type.controllers.js";
 
 const routerType = express.Router();
 
-routerType.get("/:theme/:type", getTypeController);
+routerType.get("/", getTypeController);
 
 export default routerType;
